09749231905306add94050bec204f2eeedae4ee0
Guilherme M. Schroeder <guialemas@gmail.com>
2016-03-11 14:26:59 +0100

    readme.md and install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# arch install comes here - to be done
+
+# install yaourt
+echo -e "[archlinuxfr]\nSigLevel = Never\nServer = http://repo.archlinux.fr/\$arch" >> /etc/pacman.conf
+pacman -Sy
+pacman -S yaourt --noconfirm
+
+# install and run salt
+yaourt -S salt-raet git --noconfirm
+mkdir -p /var/log/salt /srv/salt/files
+git clone https://github.com/lero/salt-arch-mac.git /srv/salt
+git clone https://github.com/lero/dotfiles.git /srv/dotfiles
+git clone https://github.com/lero/archlinux.git /srv/archlinux
+ln -s /srv/dotfiles /srv/salt/files/home
+ln -s /srv/archlinux/* /srv/salt/files/
+salt-call --local state.highstate
---
+++ readme.md
@@ -0,0 +1,6 @@
+salt-arch-mac
+-------------
+
+Salt configuration to get Arch Linux running on a MacBookPro12,1 (Early 2015).
+The idea is to run one script from Arch Linux installation and get a working box.
+Adjust `install.sh` and Salt configuration to your needs.
